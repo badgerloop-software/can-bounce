6f34c48ce6ea0ed3a721d39434f4975987013f3b
Noah Kurszewski <noahkurszewski@gmail.com>
2023-06-14 21:26:58 -0500

    Added comment with pin assignment information for each type of Nucleo.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 
-CAN canBus(PD_0, PD_1);
+CAN canBus(PD_0, PD_1);     // DEFAULT: For use with F767ZI
+//CAN canBus(PA_11, PA_12);   // For use with F303K8 or L432KC
 
 // main() runs in its own thread in the OS
 int main()
