8f9e659a47983466aa3adfbdcb34a6172627c6a1
bquan0 <powerbowen@gmail.com>
2024-09-29 11:43:45 -0500

    add canBus.begin()

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@ CAN_message_t msg;
 
 void setup() {
   canBus.setBaudRate(500000);
+  canBus.begin();
 }
 
 void loop() {
   delay(1000);
   if (canBus.read(msg)) {
     printf("Message received: %x | %d\n", msg.id, msg.buf[0]);
+  } else {
+    printf("no read");
   }
 }
