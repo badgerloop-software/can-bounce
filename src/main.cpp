25e25d4c2e81a6a016625684fc3c5fe9e177ec6a
Khiem Vu <khiemvu117@gmail.com>
2024-09-29 11:55:56 -0500

    successful CAN test

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,18 @@ STM32_CAN canBus( CAN1, ALT_2 );
 CAN_message_t msg;
 
 void setup() {
-  canBus.setBaudRate(500000);
   msg.buf[0] = 255;
   msg.id = 0x100;
   canBus.begin();
+  canBus.setBaudRate(500000);
 }
 
 void loop() {
   delay(1000);
   if (canBus.write(msg)) {
-    printf("Unsuccessful message sending\n");
-  } else {
-    msg.buf[0]--;
+    msg.buf[0] = msg.buf[0] - 1;
     printf("Successful send\n");
+  } else {
+    printf("Unsuccessful message sending\n");
   }
 }
