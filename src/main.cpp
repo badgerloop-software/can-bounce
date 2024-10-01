314c2420524c7bd5c251b130a23d61615d6c0595
bquan0 <powerbowen@gmail.com>
2024-09-30 23:19:49 -0500

    add submodule with canmanager

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,17 @@
 #include <Arduino.h>
 #include "STM32_CAN.h"
+#include "canmanager.h"
 
-STM32_CAN canBus( CAN1, ALT_2 );
+// STM32_CAN canBus( CAN1, ALT_2 );
+CANManager canBus(CAN1, ALT_2);
 
 CAN_message_t msg;
 
 void setup() {
-  canBus.begin();
-  canBus.setBaudRate(500000);
+  Serial.begin(9600);
 }
 
 void loop() {
+  Serial.println("hey");
   delay(1000);
-  if (canBus.read(msg)) {
-    printf("Message received: %x | %d\n", msg.id, msg.buf[0]);
-  } else {
-    printf("no messages in read queue\n");
-  }
 }
