05d6b40ece5bc606a36e9c3af4474e4ebe66d437
bquan0 <powerbowen@gmail.com>
2024-11-24 12:42:17 -0600

    send OV Fault reset at 0x051 and read MPPT's messages

--- src/candecoder.cpp
+++ src/candecoder.cpp
@@ -2,62 +2,23 @@
 
 #define IS_FIRST_CAN false
 
-#if IS_FIRST_CAN
-#define RECEIVE_BASE_ID 0x000
-#define SEND_BASE_ID 0x100
-#define INT8_VAL 16
-#define INT16_VAL 30271
-#define FLOAT_VAL 9.8
-#define BOOL_VAL true
-#else
-#define RECEIVE_BASE_ID 0x100
-#define SEND_BASE_ID 0x000
-#define INT8_VAL 234
-#define INT16_VAL 57219
-#define FLOAT_VAL 8.6
-#define BOOL_VAL false
-#endif
-
 uint8_t int8Received;
 uint16_t int16Received;
 float floatReceived;
 bool boolReceived;
 
-int numMessagesReceived[4] = {0,0,0,0};
+int numMessagesReceived[7] = {0,0,0,0,0,0,0};
 
 CANDecoder::CANDecoder(CAN_TypeDef* canPort, CAN_PINS pins, int frequency) : CANManager(canPort, pins, frequency){};
 
 void CANDecoder::readHandler(CAN_message_t msg) {
-   switch (msg.id) {
-      case (RECEIVE_BASE_ID + 0):
-         int8Received = *(int8_t*)msg.buf;
-         numMessagesReceived[0]++;
-         break;
-      case (RECEIVE_BASE_ID + 1):
-         int16Received = *(int16_t*)msg.buf;
-         numMessagesReceived[1]++;
-         break;
-      case (RECEIVE_BASE_ID + 2):
-         floatReceived = *(float*)msg.buf;
-         numMessagesReceived[2]++;
-         break;
-      case (RECEIVE_BASE_ID + 3):
-         boolReceived = *(bool*)msg.buf;
-         numMessagesReceived[3]++;
-         break;
-   }   
+   int offset = msg.id - 0x400;
+   if (offset >= 0 && offset <= 6) {
+      numMessagesReceived[offset]++;
+   }
 }
 
 void CANDecoder::sendSignal() {
-   uint8_t test8int = INT8_VAL; 
-   this->sendMessage(SEND_BASE_ID + 0, (void*)&test8int, sizeof(test8int));
-
-   uint16_t test16int = INT16_VAL;
-   this->sendMessage(SEND_BASE_ID + 1, (void*)&test16int, sizeof(test16int));
-
-   float testFloat = FLOAT_VAL;
-   this->sendMessage(SEND_BASE_ID + 2, (void*)&testFloat, sizeof(testFloat));
-
-   bool testBoolean = BOOL_VAL;
-   this->sendMessage(SEND_BASE_ID + 3, (void*)&testBoolean, sizeof(testBoolean));
+   uint8_t ov_high = 1;
+   this->sendMessage(0x051, (void*)&ov_high, sizeof(ov_high));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,28 @@
 #include "STM32_CAN.h"
 #include "candecoder.h"
 
-CANDecoder candecoder(CAN1, ALT_2, 500000);
+CANDecoder candecoder(CAN1, ALT_2, 250000);
 
 extern uint8_t int8Received;
 extern uint16_t int16Received;
 extern float floatReceived;
 extern bool boolReceived;
 
-extern int numMessagesReceived[4];
+extern int numMessagesReceived[7];
 
 int counter = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
   candecoder.sendSignal();
-  candecoder.runQueue(50); // 50ms, so 20 sets of messages sent per second
-  counter++;
+  printf("sent message\n");
+  candecoder.runQueue(5000);
 
-  // print out debug info every second (instead of every 50ms)
-  if (counter > 20) {
-    counter = 0;
-    Serial.println(int8Received);
-    Serial.println(int16Received);
-    Serial.println(floatReceived);
-    Serial.println(boolReceived);
-    printf("%d | %d | %d | %d\n\n", numMessagesReceived[0], numMessagesReceived[1], numMessagesReceived[2], numMessagesReceived[3]);
+  for (int i = 0; i < 7; i++) {
+    printf("%d | ", numMessagesReceived[i]);
   }
+  printf("\n");
 }
