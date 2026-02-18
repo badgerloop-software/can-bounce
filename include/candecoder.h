3b30efe7a57d8a65feb1405308066f43358e09d8
Will Wentink <wwentink@wisc.edu>
2026-02-17 18:11:58 -0600

    Full 7-board lighting system test

--- include/candecoder.h
+++ include/candecoder.h
@@ -1,50 +1,88 @@
-#ifndef __CAN_DECODER_H__
-#define __CAN_DECODER_H__
+#ifndef CANDECODER_H
+#define CANDECODER_H
 
 #include "canmanager.h"
 
+// CAN Message IDs
 #define STEERING_ID 0x300
-#define PDC_ID 0x207
+#define HAZARDS_ID 0x304
 #define BPS_ID 0x100
+#define PDC_ID 0x207
 
+// Bit positions for Steering message (0x300)
+#define STEERING_HEADLIGHT_BIT      0
+#define STEERING_LEFT_BLINK_BIT     1
+#define STEERING_RIGHT_BLINK_BIT    2
+
+// Bit positions for Hazards message (0x304)
+#define HAZARDS_BIT                 0
+
+// Bit positions for BPS message (0x100)
+#define BPS_FAULT_BIT               0
+
+// Bit positions for PDC message (0x207)
+#define PDC_BRAKE_LED_BIT           4
+
+// Data structures
 struct Steering_Data {
-   bool direction_switch : 1;      // input
-   bool left_blink : 1;            // input
-   bool right_blink : 1;           // input
-   bool crz_mode_a : 1;            // input
-   bool crz_set : 1;               // input
-   bool crz_reset : 1;             // input
-   bool horn : 1;                  // input
-   bool headlight : 1;             // input
-};
-struct PDC_Data {
-   bool direction : 1;         // output
-   bool eco_mode : 1;          // output
-   bool mc_on : 1;             // input
-   bool park_brake : 1;        // input
-   bool brakeLED : 1;          // derived by state machine
+    uint8_t headlight : 1;
+    uint8_t left_blink : 1;
+    uint8_t right_blink : 1;
+    uint8_t reserved : 5;
+    
+    // Helper to pack into byte
+    uint8_t pack() const {
+        return (headlight << STEERING_HEADLIGHT_BIT) |
+               (left_blink << STEERING_LEFT_BLINK_BIT) |
+               (right_blink << STEERING_RIGHT_BLINK_BIT);
+    }
+    
+    // Helper to unpack from byte
+    void unpack(uint8_t data) {
+        headlight = (data >> STEERING_HEADLIGHT_BIT) & 0x01;
+        left_blink = (data >> STEERING_LEFT_BLINK_BIT) & 0x01;
+        right_blink = (data >> STEERING_RIGHT_BLINK_BIT) & 0x01;
+    }
 };
 
+struct Hazards_Data {
+    uint8_t hazards : 1;
+    uint8_t reserved : 7;
+    
+    uint8_t pack() const {
+        return (hazards << HAZARDS_BIT);
+    }
+    
+    void unpack(uint8_t data) {
+        hazards = (data >> HAZARDS_BIT) & 0x01;
+    }
+};
+
+struct PDC_Data {
+    uint8_t brakeLED : 1;
+    uint8_t reserved : 7;
+    
+    uint8_t pack() const {
+        return (brakeLED << PDC_BRAKE_LED_BIT);
+    }
+    
+    void unpack(uint8_t data) {
+        brakeLED = (data >> PDC_BRAKE_LED_BIT) & 0x01;
+    }
+};
+
+extern Steering_Data steering;
+extern Hazards_Data hazards;
+extern PDC_Data pdc;
+extern uint8_t bps_fault;
 extern uint8_t num_msg_received;
 extern bool sendsuccess;
 
-extern Steering_Data steering;
-extern PDC_Data pdc;
-extern uint8_t bps_fault;
-
 class CANDecoder : public CANManager {
 public:
-
-   CANDecoder(CAN_TypeDef* canPort, CAN_PINS pins, int frequency = DEFAULT_CAN_FREQ);
-
-   /* Reads input message and does any logic handling needed
-      * Intended to be implemented by class extension per board
-      */
-   void readHandler(CAN_message_t msg);
-
-   /* Send all of this board's message over CAN
-      */ 
-   void sendSignal();
+    CANDecoder(CAN_TypeDef* canPort, CAN_PINS pins, int frequency);
+    void readHandler(CAN_message_t msg) override;
+    void sendSignal();
 };
 
-#endif
+#endif // CANDECODER_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,10 @@
 #include "STM32_CAN.h"
 #include "candecoder.h"
 
-
 CANDecoder candecoder(CAN1, ALT_2, 250000);
 
 char msg;
 uint8_t counter = 0;
-
 
 void setup() {
   Serial.begin(115200);
@@ -34,15 +32,29 @@ void loop() {
       case '4':
         pdc.brakeLED = !pdc.brakeLED;
         break;
-      default:
+      case '5':
         bps_fault = (bps_fault == 0)? 1 : 0;
+        break;
+      case '6':
+        // Toggle hazards by setting both blinkers
+        if (steering.left_blink && steering.right_blink) {
+          // Turn off hazards
+          steering.left_blink = 0;
+          steering.right_blink = 0;
+        } else {
+          // Turn on hazards
+          steering.left_blink = 1;
+          steering.right_blink = 1;
+        }
+        break;
+      default:
         break;
     }
   }
   candecoder.runQueue(1000);
   candecoder.sendSignal();
   counter++;
-  if (counter >=1) {
+  if (counter >= 1) {
     counter = 0;
     printf("\033[2J\033[1;1H");
     printf("1) Left Blink: %s\n", steering.left_blink? "on" : "off");
@@ -50,9 +62,9 @@ void loop() {
     printf("3) Headlight: %s\n", steering.headlight? "on" : "off");
     printf("4) Brake Light: %s\n", pdc.brakeLED? "on" : "off");
     printf("5) BPS Fault: %s\n", bps_fault? "on" : "off");
+    printf("6) Hazards: %s\n", hazards.hazards? "on" : "off");
     printf("sendsuccess: %s\n", sendsuccess? "yes" : "no");
     printf("messages received: %d\n", num_msg_received);
-    printf("steering: %x\n", steering);
+    printf("steering raw: 0x%02X\n", steering.pack());
   }
-
 }
